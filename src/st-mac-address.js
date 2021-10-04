d3bff7e475b8010610f0e3283654a0294fe06fc5
komoskoann\\----------- <komoskoann@gmail.com>
2021-10-05 02:11:49 +0300

    feat: solve task

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -15,5 +15,7 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function isMAC48Address(n) {
-  
+  const validSymbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+  return n.split('-').every(element => element.length === 2 && element.split('').every(el => validSymbols.includes(el)))
 }
