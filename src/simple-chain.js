582779cfc4605d1de12d1162b54eca88b8b44ebd
komoskoann\\----------- <komoskoann@gmail.com>
2021-10-05 01:49:42 +0300

    feat: solve task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,27 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+  stack: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.stack.push(value)
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (!Number.isInteger(position) || !this.stack[position - 1]) {
+      this.stack.length = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.stack.splice(position - 1, 1)
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.stack.reverse();
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.map(a => `( ${a} )`).join('~~')
   }
 };
