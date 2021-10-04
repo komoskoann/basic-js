af0b837d096fc562fe57f441f6ac2c12717071a9
komoskoann\\----------- <komoskoann@gmail.com>
2021-10-05 01:01:55 +0300

    feat: solve task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let result = [];
+  for(let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'number') {
+      result.push(arr[i]);
+    } else if (arr[i] === '--double-next' && arr[i + 1]) {
+      result.push(arr[i + 1], arr[i + 1])
+    } else if (arr[i] === '--double-prev' && result.length > 0) {
+      let previousElement = result[result.length - 1]
+      result.push(previousElement, previousElement);
+    } else if (arr[i] === '--discard-prev' && result.length > 0) {
+      result.pop();
+    } else if (arr[i] === '--discard-next' && arr[i + 1]) { 
+      i++;
+    }
+  }
+  return result;
 }
