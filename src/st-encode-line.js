2d198d9843d85376c48f9d0b6c1f9cdd0af4a647
komoskoann\\----------- <komoskoann@gmail.com>
2021-10-05 02:23:44 +0300

    feat: solve task

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -11,5 +11,31 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function encodeLine(str) {
-  
+  if (str === '') {
+    return '';
+  }
+  let newLine = ''
+  let splittedString = str.split('');
+  let currentElement = splittedString[0];
+  let n = 1;
+  splittedString.slice(1, splittedString.length).forEach(function(element) {
+    if (currentElement == element) {
+      n++;
+    } else if (n > 1) {
+      newLine += n + currentElement;
+      currentElement = element;
+      n = 1;
+    } else {
+      newLine += currentElement;
+      currentElement = element;
+      n = 1
+    }
+  })
+
+  if (n > 1) {
+    newLine += n + currentElement;
+  } else {
+    newLine += currentElement;
+  }
+  return newLine;
 }
