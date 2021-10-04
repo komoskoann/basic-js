f547cf6d15e201ec0db3ac72ea49c6c0d2fceed5
komoskoann\\----------- <komoskoann@gmail.com>
2021-10-05 01:57:26 +0300

    feat: solve task

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -22,7 +22,6 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats(domains) {
+  
 }
--- src/st-matrix-elements-sum.js
+++ src/st-matrix-elements-sum.js
@@ -16,7 +16,6 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be 9
  */
-export default function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getMatrixElementsSum(matrix) {
+ 
 }
